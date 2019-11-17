51a9f422f20e1aaf73897c004587cabd49d210fb
huntercoad <55352425+huntercoad@users.noreply.github.com>
2019-11-17 18:01:06 -0500

    Add files via upload

---
+++ Scripts/WeaponController.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class WeaponController : MonoBehaviour
+{
+    public AudioSource audioSource;
+    public AudioClip weapon_enemy;
+    public GameObject shot;
+    public Transform shotSpawn;
+    public float fireRate;
+    public float delay;
+
+    
+
+    void Start()
+    {
+        InvokeRepeating("Fire", delay, fireRate);
+    }
+
+    void Fire()
+    {
+        Instantiate(shot, shotSpawn.position, shotSpawn.rotation);
+        audioSource.clip = weapon_enemy;
+        audioSource.Play();
+    }
+}
